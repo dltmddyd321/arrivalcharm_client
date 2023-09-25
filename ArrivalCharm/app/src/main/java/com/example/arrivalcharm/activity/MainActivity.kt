86a04aeb64cb54a3693543f8d92f398f65566026
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-09-24 20:27:24 -0700

    카카오 계정 id 수신 후, Main 서버 로그인 처리 구성

--- ArrivalCharm/app/src/main/java/com/example/arrivalcharm/activity/MainActivity.kt
+++ ArrivalCharm/app/src/main/java/com/example/arrivalcharm/activity/MainActivity.kt
@@ -121,16 +121,42 @@ class MainActivity : AppCompatActivity() {
     private fun requestKakaoLogin() {
         val kakaoCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
-                Toast.makeText(this@MainActivity, "네이버 아이디 로그인 실패!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, "카카오 아이디 로그인 실패!", Toast.LENGTH_SHORT).show()
             } else if (token != null) {
-                Toast.makeText(this@MainActivity, "네이버 아이디 로그인 성공! 1 -> ${token.accessToken}", Toast.LENGTH_SHORT).show()
+                UserApiClient.instance.me { user, err ->
+                    if (err != null) return@me
+                    Toast.makeText(this@MainActivity, "카카오 아이디 로그인 성공! 1 -> ${user?.id}", Toast.LENGTH_SHORT).show()
+                    val loginObject = UserLoginInfo(
+                        LoginType.KAKAO.name.lowercase(),
+                        user?.id.toString(),
+                        "이승용",
+                        "1q2w3e4r!",
+                        "dinoinventor@naver.com"
+                    )
+                    lifecycleScope.launch {
+                        loginViewModel.startLogin(loginObject).collect { result ->
+                            when (result) {
+                                is ApiResult.Success -> Toast.makeText(
+                                    this@MainActivity,
+                                    result.data,
+                                    Toast.LENGTH_LONG
+                                ).show()
+                                is ApiResult.Error -> Toast.makeText(
+                                    this@MainActivity,
+                                    result.message,
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+                    }
+                }
             }
         }
 
         if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
             UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
                 if (error != null) {
-                    Toast.makeText(this@MainActivity, "네이버 아이디 로그인 실패!", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@MainActivity, "카카오 아이디 로그인 실패!", Toast.LENGTH_SHORT).show()
                     // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
                     // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
                     if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
@@ -139,7 +165,7 @@ class MainActivity : AppCompatActivity() {
                     // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
                     UserApiClient.instance.loginWithKakaoAccount(this, callback = kakaoCallback)
                 } else if (token != null) {
-                    Toast.makeText(this@MainActivity, "네이버 아이디 로그인 성공! 2 -> ${token.accessToken}", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@MainActivity, "카카오 아이디 로그인 성공! 2 -> ${token.accessToken}", Toast.LENGTH_SHORT).show()
                 }
             }
         } else {
