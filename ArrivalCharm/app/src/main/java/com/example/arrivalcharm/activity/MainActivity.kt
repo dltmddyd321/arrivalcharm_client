39527e5bd173012df3130949eef2f52e5cc1ad45
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-10-09 18:56:12 -0700

    Splash Api 적용

--- ArrivalCharm/app/src/main/java/com/example/arrivalcharm/activity/MainActivity.kt
+++ ArrivalCharm/app/src/main/java/com/example/arrivalcharm/activity/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.arrivalcharm.activity
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.AnticipateInterpolator
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import com.example.arrivalcharm.databinding.ActivityMainBinding
-import com.example.arrivalcharm.datamodel.Location
 import com.example.arrivalcharm.db.datastore.DatastoreViewModel
 import com.example.arrivalcharm.db.room.LocationViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -19,12 +29,17 @@ import kotlinx.coroutines.withContext
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var splash: SplashScreen
     private lateinit var binding: ActivityMainBinding
     private val locationViewModel: LocationViewModel by viewModels()
     private val dataStoreViewModel: DatastoreViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        splash = installSplashScreen()
+        startSplash()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -68,4 +83,77 @@ class MainActivity : AppCompatActivity() {
 //            }
 //        }
     }
+
+    @SuppressLint("Recycle")
+    private fun startSplash() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.setOnExitAnimationListener { splashView ->
+                val icon = splashView.iconView ?: return@setOnExitAnimationListener
+
+                ObjectAnimator.ofPropertyValuesHolder(icon).run {
+                    interpolator = AnticipateInterpolator()
+                    repeatCount = 2 // 반복 횟수
+                    duration = 500L
+                    doOnEnd {
+                        splashView.remove()
+                    }
+                    start()
+                }
+            }
+        }
+    }
+
+
+//    private fun startSplash() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+//            splashScreen.setOnExitAnimationListener { splashView ->
+//                val fadeIn = AlphaAnimation(0f, 1f)
+//                fadeIn.duration = 1000
+//
+//                val fadeOut = AlphaAnimation(1f, 0f)
+//                fadeOut.startOffset = 1000
+//                fadeOut.duration = 1000
+//
+//                val animationSet = AnimationSet(true)
+//                animationSet.addAnimation(fadeIn)
+//                animationSet.addAnimation(fadeOut)
+//
+//                splashView.iconView?.startAnimation(animationSet)
+//
+//                animationSet.setAnimationListener(object : Animation.AnimationListener {
+//                    override fun onAnimationStart(animation: Animation?) {
+//                    }
+//
+//                    override fun onAnimationEnd(animation: Animation?) {
+//                        splashView.remove()
+//                    }
+//
+//                    override fun onAnimationRepeat(animation: Animation?) {}
+//                })
+//            }
+//        }
+//    }
+
+//    @SuppressLint("Recycle")
+//    private fun startSplash() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+//            splashScreen.setOnExitAnimationListener { splashView ->
+//                val icon = splashView.iconView ?: return@setOnExitAnimationListener
+//                icon.rotation = 90f
+//
+//                ObjectAnimator.ofFloat(
+//                    icon,
+//                    "rotation",
+//                    0f
+//                ).apply {
+//                    interpolator = AnticipateInterpolator()
+//                    duration = 1000L
+//                    doOnEnd {
+//                        splashView.remove()
+//                    }
+//                    start()
+//                }
+//            }
+//        }
+//    }
 }
