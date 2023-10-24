8a5b3973397a6d1e1cfdf326603178ede49c861a
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-10-24 22:03:18 +0900

    레이아웃 작업

--- ArrivalCharm/app/src/main/java/com/example/arrivalcharm/fragment/MapFragment.kt
+++ ArrivalCharm/app/src/main/java/com/example/arrivalcharm/fragment/MapFragment.kt
@@ -6,12 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.arrivalcharm.R
+import com.example.arrivalcharm.databinding.FragmentMapBinding
 
 class MapFragment : Fragment() {
+
+    private lateinit var binding: FragmentMapBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_map, container, false)
+    ): View {
+        binding = FragmentMapBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
