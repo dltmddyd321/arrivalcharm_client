3f09be80ff6b2a851a3efec7e5e26f743d4fea02
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-10-29 18:39:57 +0900

    프래그먼트 단위로 Map 이전

--- ArrivalCharm/app/src/main/java/com/example/arrivalcharm/fragment/MapFragment.kt
+++ ArrivalCharm/app/src/main/java/com/example/arrivalcharm/fragment/MapFragment.kt
@@ -1,16 +1,32 @@
 package com.example.arrivalcharm.fragment
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import com.example.arrivalcharm.R
 import com.example.arrivalcharm.databinding.FragmentMapBinding
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var binding: FragmentMapBinding
+    private var map: GoogleMap? = null
+    private var mFusedLocationProviderClient: FusedLocationProviderClient? = null
+    lateinit var mLastLocation: Location
+    private lateinit var mLocationRequest: LocationRequest
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +39,49 @@ class MapFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val mapFragment: SupportMapFragment? =
+            childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        mLocationRequest = LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 5000
+            fastestInterval = 2000
+        }
+        startLocationUpdate()
     }
+
+    private fun startLocationUpdate() {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        mFusedLocationProviderClient?.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.getMainLooper())
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            locationResult.lastLocation?.let { onLocationChanged(it) }
+        }
+    }
+
+    private fun onLocationChanged(location: Location) {
+        mLastLocation = location
+
+        val seoul = LatLng(location.latitude, location.longitude)
+
+        val markerOptions = MarkerOptions()
+        markerOptions.position(seoul)
+        markerOptions.title("서울")
+        markerOptions.snippet("한국 수도")
+
+        map?.addMarker(markerOptions)
+
+        map?.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 10f))
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) { map = googleMap }
 }
