f3af8416f905b3603378dd6f094c6ed0c583c8e5
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-12-05 21:22:07 +0900

    Composable AlertDialog 구현

--- ArrivalCharm/app/src/main/java/com/example/arrivalcharm/activity/AlarmSettingActivity.kt
+++ ArrivalCharm/app/src/main/java/com/example/arrivalcharm/activity/AlarmSettingActivity.kt
@@ -4,23 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.lifecycleScope
 import com.example.arrivalcharm.activity.ui.theme.ArrivalCharmTheme
 import com.example.arrivalcharm.db.datastore.DatastoreViewModel
-import kotlinx.coroutines.launch
 
 class AlarmSettingActivity : ComponentActivity() {
 
@@ -44,10 +38,51 @@ class AlarmSettingActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Row {
-        Text("알람 거리 :")
-        Spacer(modifier = Modifier.width(10.dp))
-        Text("2")
+    Column {
+        Row {
+            Text("알람 거리 :")
+            Spacer(modifier = Modifier.width(10.dp))
+            Text("2")
+        }
+        Spacer(modifier = Modifier.size(10.dp))
+        AlertDialogShow()
+    }
+}
+
+@Composable
+fun AlertDialogShow() {
+    val showDialog = remember { mutableStateOf(false) }
+
+    Button(onClick = {
+        showDialog.value = true
+    }) {
+        Text(text = "수정하기")
+    }
+
+    if (showDialog.value) {
+        AlertDialog(
+            onDismissRequest = { showDialog.value = false },
+            title = { Text(text = "알람 거리 수정") },
+            text = { Text(text = "작업을 진행하시겠습니까?") },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        showDialog.value = false
+                        // 확인 동작
+                    }) {
+                    Text("확인")
+                }
+            },
+            dismissButton = {
+                Button(
+                    onClick = {
+                        showDialog.value = false
+                        // 취소 동작
+                    }) {
+                    Text("취소")
+                }
+            }
+        )
     }
 }
 
