d4a505a93cd18ece97a30c4e3463cae91a37a937
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-01-19 21:53:45 +0900

    알람 거리 수정 Text Compose로 입력 처리

--- ArrivalCharm/app/src/main/java/com/example/arrivalcharm/activity/AlarmSettingActivity.kt
+++ ArrivalCharm/app/src/main/java/com/example/arrivalcharm/activity/AlarmSettingActivity.kt
@@ -1,19 +1,25 @@
 package com.example.arrivalcharm.activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.arrivalcharm.activity.ui.theme.ArrivalCharmTheme
 import com.example.arrivalcharm.db.datastore.DatastoreViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class AlarmSettingActivity : ComponentActivity() {
 
@@ -28,7 +34,7 @@ class AlarmSettingActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = Color.White
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -36,21 +42,33 @@ class AlarmSettingActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun Greeting() {
+
+    var alarmDistanceText by remember {
+        mutableStateOf("2")
+    }
+
     Column {
         Row {
             Text("알람 거리 :")
             Spacer(modifier = Modifier.width(10.dp))
-            Text("2")
+            Text(alarmDistanceText)
         }
         Spacer(modifier = Modifier.size(10.dp))
-        AlertDialogShow()
+        AlertDialogShow {
+            alarmDistanceText = it
+        }
     }
 }
 
 @Composable
-fun AlertDialogShow() {
+fun AlertDialogShow(onClickConfirm: (String) -> Unit) {
     val showDialog = remember { mutableStateOf(false) }
+    var editTextValue by remember { mutableStateOf(TextFieldValue()) }
+    val scaffoldState = rememberScaffoldState()
+    var titleText by remember {
+        mutableStateOf("알람 거리 수정")
+    }
 
     Button(onClick = {
         showDialog.value = true
@@ -59,77 +77,76 @@ fun AlertDialogShow() {
     }
 
     if (showDialog.value) {
-        AlertDialog(
-            onDismissRequest = { showDialog.value = false },
-            title = { Text(text = "알람 거리 수정") },
-            text = { Text(text = "작업을 진행하시겠습니까?") },
-            confirmButton = {
-                Button(
-                    onClick = {
-                        showDialog.value = false
-                        // 확인 동작
-                    }) {
-                    Text("확인")
+        val coroutineScope = rememberCoroutineScope()
+        Scaffold(scaffoldState = scaffoldState, modifier = Modifier.fillMaxSize()) {
+            AlertDialog(
+                onDismissRequest = {
+                    showDialog.value = false
+                    editTextValue = TextFieldValue()
+                },
+                title = { Text(text = titleText) },
+                text = {
+                    BasicTextField(value = editTextValue, onValueChange = {
+                        editTextValue = it
+                    })
+                },
+                confirmButton = {
+                    Button(
+                        onClick = {
+                            val updateValue = editTextValue.text
+                            if (isConvertibleToInt(updateValue)) {
+                                onClickConfirm.invoke(updateValue)
+                                showDialog.value = false //dismiss
+                                editTextValue = TextFieldValue()
+                            } else {
+                                coroutineScope.launch {
+                                    titleText = "숫자 형식을 입력하세요."
+                                    delay(2000)
+                                    titleText = "알람 거리 수정"
+                                }
+                            }
+                        }) {
+                        Text("확인")
+                    }
+                },
+                dismissButton = {
+                    Button(
+                        onClick = {
+                            showDialog.value = false
+                            editTextValue = TextFieldValue()
+                        }) {
+                        Text("취소")
+                    }
+                }, modifier = Modifier.background(Color.White)
+            )
+        }
+
+        SnackbarHost(
+            scaffoldState.snackbarHostState,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            snackbar = {
+                Snackbar(action = {
+                    TextButton(onClick = { scaffoldState.snackbarHostState.currentSnackbarData?.dismiss() }) {
+                        Text(text = "닫기")
+                    }
+                }, modifier = Modifier.background(Color.White)) {
+                    Text(
+                        text = scaffoldState.snackbarHostState.currentSnackbarData?.message
+                            ?: "숫자 형태 값을 입력하세요."
+                    )
                 }
-            },
-            dismissButton = {
-                Button(
-                    onClick = {
-                        showDialog.value = false
-                        // 취소 동작
-                    }) {
-                    Text("취소")
-                }
-            }
-        )
+            })
     }
 }
 
-@Composable
-fun DistanceInputDialog(
-    showDialog: MutableState<Boolean>,
-    onConfirm: (String) -> Unit,
-    onDismiss: () -> Unit
-) {
-    var text by remember { mutableStateOf(TextFieldValue()) }
-
-    if (showDialog.value) {
-        AlertDialog(
-            onDismissRequest = { onDismiss() },
-            title = { Text("알람 거리 수정") },
-            text = {
-                Column {
-                    Text("작업을 진행하시겠습니까?")
-                    TextField(
-                        value = text,
-                        onValueChange = {
-                            text = it
-                        },
-                        label = { Text("텍스트 입력") }
-                    )
-                }
-            },
-            confirmButton = {
-                Button(
-                    onClick = {
-                        showDialog.value = false
-                        onConfirm(text.text)
-                    }
-                ) {
-                    Text("확인")
-                }
-            },
-            dismissButton = {
-                Button(
-                    onClick = {
-                        showDialog.value = false
-                        onDismiss()
-                    }
-                ) {
-                    Text("취소")
-                }
-            }
-        )
+private fun isConvertibleToInt(text: String): Boolean {
+    return try {
+        text.toInt()
+        true
+    } catch (e: java.lang.NumberFormatException) {
+        false
     }
 }
 
@@ -137,6 +154,6 @@ fun DistanceInputDialog(
 @Composable
 fun DefaultPreview() {
     ArrivalCharmTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
