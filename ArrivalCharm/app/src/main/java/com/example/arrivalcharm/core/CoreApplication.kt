0a769209113960ef6092032b75775311803b5987
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-04-15 20:38:17 +0900

    AppCore 생명주기 체크

--- ArrivalCharm/app/src/main/java/com/example/arrivalcharm/core/CoreApplication.kt
+++ ArrivalCharm/app/src/main/java/com/example/arrivalcharm/core/CoreApplication.kt
@@ -1,9 +1,13 @@
 package com.example.arrivalcharm.core
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
+import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
 import com.navercorp.nid.NaverIdLoginSDK
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class CoreApplication : Application() {
@@ -16,5 +20,36 @@ class CoreApplication : Application() {
             AppConst.NAVER_CLIENT_SECRET,
             "ArrivalCharm"
         )
+        registerActivityLifecycleCallbacks(AppLifecycleCallbacks())
+    }
+
+    private inner class AppLifecycleCallbacks : ActivityLifecycleCallbacks {
+        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+            Timber.tag("Lifecycle").d("Created: " + activity.localClassName)
+        }
+
+        override fun onActivityStarted(activity: Activity) {
+            Log.d("Lifecycle", "Started: ${activity.localClassName}")
+        }
+
+        override fun onActivityResumed(activity: Activity) {
+            Log.d("Lifecycle", "Resumed: ${activity.localClassName}")
+        }
+
+        override fun onActivityPaused(activity: Activity) {
+            Log.d("Lifecycle", "Paused: ${activity.localClassName}")
+        }
+
+        override fun onActivityStopped(activity: Activity) {
+            Log.d("Lifecycle", "Stopped: ${activity.localClassName}")
+        }
+
+        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+            Log.d("Lifecycle", "SaveInstanceState: ${activity.localClassName}")
+        }
+
+        override fun onActivityDestroyed(activity: Activity) {
+            Log.d("Lifecycle", "Destroyed: ${activity.localClassName}")
+        }
     }
 }
