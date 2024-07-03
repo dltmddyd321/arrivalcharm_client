75970a8835583d717287ac9a6c418901a1aba056
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2024-07-03 22:17:36 +0900

    Update RecentViewModel.kt

--- ArrivalCharm/app/src/main/java/com/example/arrivalcharm/db/room/RecentViewModel.kt
+++ ArrivalCharm/app/src/main/java/com/example/arrivalcharm/db/room/RecentViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.arrivalcharm.db.room
 
-class RecentViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.arrivalcharm.datamodel.Recent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@HiltViewModel
+class RecentViewModel @Inject constructor(
+    private val recentRepository: RecentRepository
+) : ViewModel() {
+    suspend fun getAllRecent(): List<Recent> {
+        return withContext(viewModelScope.coroutineContext) {
+            recentRepository.getAllRecent()
+        }
+    }
+
+    fun updateRecent(recent: Recent) {
+        recentRepository.updateRecent(recent)
+    }
+
+    fun deleteRecent(recent: Recent) {
+        recentRepository.deleteRecent(recent)
+    }
 }
